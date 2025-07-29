aea288c3ff0b8a0df54d18c671f3ca8a2e2e5698
Zac <zachary123roberts@gmail.com>
2025-07-28 22:23:57 -0500

    feat: added profile and settings page, wired home page buttons

--- app/src/main/java/com/example/binbuddy/MainActivity.kt
+++ app/src/main/java/com/example/binbuddy/MainActivity.kt
@@ -4,17 +4,33 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        window.decorView.setOnApplyWindowInsetsListener { view, insets ->
+            view.setPadding(0, 0, 0, 0)
+            insets
+        }
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.storesButton).setOnClickListener {
-            val intent = Intent(this, StoreActivity::class.java)
-            startActivity(intent)
+        val storesBtn = findViewById<Button>(R.id.storesButton)
+        val profileBtn = findViewById<Button>(R.id.profileButton)
+        val settingsBtn = findViewById<Button>(R.id.settingsButton)
+
+        storesBtn.setOnClickListener {
+            startActivity(Intent(this, StoreActivity::class.java))
         }
 
+        profileBtn.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
+
+        settingsBtn.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
     }
 }
