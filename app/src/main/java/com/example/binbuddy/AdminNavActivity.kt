0534d90bae029fc06dd2ccf298dfd8dfc9c89999
Elly Boykin <ellyboykinblueberry@gmail.com>
2025-08-04 14:20:15 -0500

    Added basic admin navigation

---
+++ app/src/main/java/com/example/binbuddy/AdminNavActivity.kt
@@ -0,0 +1,12 @@
+package com.example.binbuddy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AdminNavActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.admin_navigation)
+    }
+}
--- app/src/main/java/com/example/binbuddy/ProfileActivity.kt
+++ app/src/main/java/com/example/binbuddy/ProfileActivity.kt
@@ -1,11 +1,19 @@
 package com.example.binbuddy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val adminNav = findViewById<Button>(R.id.adminnav)
+
+        adminNav.setOnClickListener {
+            startActivity(Intent(this, AdminNavActivity::class.java))
+        }
     }
 }
